edb5bc1b7d5cb4b62900cc0e73eef123192612e7
Passakorn Rattanaprapan <darklink@localhost.localdomain>
2017-11-25 06:41:03 -0500

    Add user page

--- src/app/home/containers/user/user.component.ts
+++ src/app/home/containers/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router'
+import { DbService } from '../../../shared/services/db.service'
+import User from '../../../shared/models/user.model'
 
 @Component({
   selector: 'app-user',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
+  public user: User
+  constructor (
+    private dbService: DbService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
-  constructor() { }
+  ngOnInit () {
+    this.activatedRoute
+    .paramMap
+    .subscribe((params: ParamMap) => {
+      this.findUser(params.get('id'))
+    })
+  }
 
-  ngOnInit() {
+  async findUser (userId: string) {
+    const majorId = userId.slice(0, 2).toLowerCase()
+    const interviewGroup = await this.dbService.getInterviewGroup().toPromise()
+    this.user = interviewGroup[majorId].find((user: User) => {
+      return user.interviewRef === userId
+    })
+    console.log(this.user)
   }
 
 }
--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -2,5 +2,5 @@ export default class User {
     firstName: string
     lastName: string
     major: string
-    interviewRef: 'CT' | 'DS' | 'MK' | 'PG' 
+    interviewRef: string
 }
