f1679bafe1bfd39402ffb52fbf39480b2434128e
Passakorn Rattanaprapan <darklink@localhost.localdomain>
2017-11-25 18:42:22 -0500

    Add score and update user-row desing

--- src/app/home/components/user-row/user-row.component.ts
+++ src/app/home/components/user-row/user-row.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core'
+import { InterviewService } from '../../../shared/services/interview.service'
 import User from '../../../shared/models/user.model'
-
 @Component({
   selector: 'mc-user',
   templateUrl: './user-row.component.html',
@@ -11,12 +11,17 @@ export class UserRowComponent implements OnInit {
   @Input('user') user: User
 
   public shareUrl = ''
-  constructor() { }
+  constructor (
+    private interviewService: InterviewService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit () {
     const userId = this.user.interviewRef
     const majorId = userId.slice(0, 2)
     this.shareUrl = `http://${location.host}/${majorId}/${userId}`
   }
 
+  addScore () {
+    this.interviewService.addScore(this.user.key, this.user.score + 1)
+  }
 }
