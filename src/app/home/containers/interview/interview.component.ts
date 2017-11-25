f97b97b4d6daf11d07ec8d92fde3cc1d367de8c0
Passakorn Rattanaprapan <darklink@localhost.localdomain>
2017-11-25 01:30:56 -0500

    Change css to scss

--- src/app/home/containers/interview/interview.component.ts
+++ src/app/home/containers/interview/interview.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-interview',
   templateUrl: './interview.component.html',
-  styleUrls: ['./interview.component.css']
+  styleUrls: ['./interview.component.scss']
 })
 export class InterviewComponent implements OnInit {
 
