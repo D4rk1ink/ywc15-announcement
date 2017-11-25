04889e2c1b26349714c50e844835f867dfff33d3
Passakorn Rattanaprapan <darklink@localhost.localdomain>
2017-11-25 11:14:08 -0500

    Update design search component

--- src/app/home/components/search/search.component.ts
+++ src/app/home/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 import User from '../../../shared/models/user.model'
 
 @Component({
@@ -6,15 +6,35 @@ import User from '../../../shared/models/user.model'
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnChanges {
 
+  @Input('major') major: string
   @Input('users') users: User[]
   @Output('search') search: EventEmitter<any>
   
-  public searchText: string
+  public placeholderText: string = ''
+  public searchText: string = ''
 
   constructor () {
     this.search = new EventEmitter<any>()
+  }
+
+  ngOnChanges () {
+    if (this.users) {
+      this.onSearchChange(this.searchText)
+    }
+    if (this.major) {
+      switch (this.major) {
+        case 'CT':
+          return this.placeholderText = 'web content'
+        case 'DS':
+          return this.placeholderText = 'web design'
+        case 'MK':
+          return this.placeholderText = 'web marketing'
+        case 'PG':
+          return this.placeholderText = 'web programming'
+      }
+    }
   }
  
   ngOnInit () {
@@ -24,7 +44,7 @@ export class SearchComponent implements OnInit {
   onSearchChange (name) {
     const users = this.users.filter((user: User) => {
       const fullName = `${user.firstName} ${user.lastName}`
-      return new RegExp(`${name}`).test(fullName)
+      return new RegExp(`${this.searchText}`).test(fullName)
     })
     this.search.emit(users)
   }
