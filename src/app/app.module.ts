9b7e3ce420d6dd86b35e90004625bbc9e4c2ca20
Passakorn Rattanaprapan <darklink@localhost.localdomain>
2017-11-25 11:35:44 -0500

    Apply firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module'
 import { SharedModule } from './shared/shared.module'
 import { HttpClientModule } from '@angular/common/http'
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
 
+import { environment } from 'environments/environment'
+
+var firebaseConfig = {
+  apiKey: environment.apiKey,
+  authDomain: environment.authDomain,
+  databaseURL: environment.databaseURL,
+  projectId: environment.projectId,
+  storageBucket: environment.storageBucket,
+  messagingSenderId: environment.messagingSenderId,
+};
 
 @NgModule({
   declarations: [
@@ -13,6 +25,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),  // Add this
+    AngularFirestoreModule,
     HttpClientModule,
     SharedModule,
     AppRoutingModule
