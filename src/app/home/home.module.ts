af85c486e947594a6c862546d46f51600e917766
Passakorn Rattanaprapan <darklink@localhost.localdomain>
2017-11-25 02:05:48 -0500

    Add home-routing

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeRoutingModule } from './home-routing.module'
+
 import { InterviewComponent } from './containers/interview/interview.component';
 import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HomeRoutingModule
   ],
   declarations: [
     InterviewComponent,
