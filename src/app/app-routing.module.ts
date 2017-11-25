3852bc7a5aa948c4096c18eca3fb923ffdbed792
Passakorn Rattanaprapan <darklink@localhost.localdomain>
2017-11-25 06:39:29 -0500

    Change routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'home', loadChildren: 'app/home/home.module#HomeModule' },
+    { path: '', loadChildren: 'app/home/home.module#HomeModule' },
 ];
 
 @NgModule({
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'
 import { InterviewComponent } from './containers/interview/interview.component'
+import { UserComponent } from './containers/user/user.component'
 
 const routes: Routes = [
   {
-    path: '', component: InterviewComponent, 
+    path: 'interview', component: InterviewComponent, 
+  },
+  {
+    path: 'ct/:id', component: UserComponent, 
+  },
+  {
+    path: 'ds/:id', component: UserComponent, 
+  },
+  {
+    path: 'mk/:id', component: UserComponent, 
+  },
+  {
+    path: 'pg/:id', component: UserComponent, 
+  },
+  {
+    path: '**', redirectTo: 'interview'
   }
 ];
 
